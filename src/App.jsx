2506360c0f2c5dbcd17c48ee8e89d590201fa895
ybondev <ybon.dev@gmail.com>
2023-02-22 20:16:34 +0800

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.scss";
 import Navbar from "./components/Navbar";
 
+// ICONS
+import {BsChevronRight} from "react-icons/bs";
+
 function App() {
   return (
+    <>
     <div className="netflix-container">
-      <Navbar />
+    <Navbar />
       <div className="bg-netflix">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/83e8c151-107d-4e8f-b95a-d2ba99d49bb9/370eabfb-2db4-4a63-ba8c-9e1821d6de68/PH-en-20230213-popsignuptwoweeks-perspective_alpha_website_large.jpg"
@@ -13,7 +17,30 @@ function App() {
         />
         <div className="img-top-overlay"></div>
       </div>
-      <div className="container">
+      <div className="container first-container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="text-content">
+              <h1 className="title">
+                Unlimited movies, TV <br/> shows, and more.
+              </h1>
+              <h2 className="my-3">
+                Watch anywhere. Cancel anytime.
+              </h2>
+              <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+              <form action="" className="mt-3">
+                <div className="form-floating">
+                  <input type="email" id="floatingInput" className="form-control" placeholder="Email address" required/>
+                  <button class="input-group-text" id="basic-addon2">Get Started               <BsChevronRight/></button>
+                  <label htmlFor="floatingInput">Email address</label>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    {/* <div className="container">
         <div className="row">
           <div className="col-md-6">
             <div className="first-row-content">
@@ -33,8 +60,8 @@ function App() {
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </div> */}
+    </>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,25 +5,25 @@ import logo from "../assets/Netflix_Logo_PMS.png";
 
 const Navbar = () => {
   return (
-    <nav className="container py-4">
-      <div className="logo">
-        <img src={logo} alt="Netflix logo" className="img-fluid" />
+    <nav className="py-4">
+      <div className="container">
+        <div className="logo">
+          <img src={logo} alt="Netflix logo" className="img-fluid" />
+        </div>
+        <ul>
+          <li className="link-option me-3">
+            <select>
+              <option value="English">English</option>
+              <option value="Filipino">Filipino</option>
+            </select>
+          </li>
+          <li className="link-sign-in">
+            <a href="#" className="sign-in">
+              Sign in
+            </a>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li className="link-option me-3">
-          <select>
-            <option value="English">
-              English
-            </option>
-            <option value="Filipino">Filipino</option>
-          </select>
-        </li>
-        <li className="link-sign-in">
-          <a href="#" className="sign-in">
-            Sign in
-          </a>
-        </li>
-      </ul>
     </nav>
   );
 };
